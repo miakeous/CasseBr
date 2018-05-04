1fea062e38aee795563afd538df4d5e5afd65475
Unknown <manon.portay2@gmail.com>
2018-05-04 10:42:22 +0200

    J'ai oublié lui

--- mur.h
+++ mur.h
@@ -1,24 +1,23 @@
 #ifndef MUR_H
 #define MUR_H
-
+/*
 //include Pierre (mac)
 
 #include "glu.h"
 #include <GLUT/glut.h>
 
-
+*/
 
 //include Manon (windows)
-/*#include <GL/glu.h>
+#include <GL/glu.h>
 #include <GL/gl.h>
-*/
 
 #include <QDebug>
 
 class mur
 {
 public:
-    mur(int X, int Z, int height, int width, QString rot);
+    mur(int X, int Z, int height, int width, QString rot, bool bas);
     void display();
     void rectangle(GLfloat posX, GLfloat posY, GLfloat posZ, GLfloat profondeur, GLfloat hauteur, GLfloat taille);
     int getPosx(){return posX;}
@@ -29,6 +28,7 @@ private:
     int posZ;
     int hauteur;
     int largeur;
+    bool murbas;
     QString tourne;
 };
 
