02093d1ffd2733efabdfbf133738168451fdf7b2
Unknown <manon.portay2@gmail.com>
2018-05-04 12:29:32 +0200

    cassebrique

---
+++ joueur.cpp
@@ -0,0 +1,6 @@
+#include "joueur.h"
+
+joueur::joueur()
+{
+
+}
---
+++ joueur.h
@@ -0,0 +1,25 @@
+#ifndef JOUEUR_H
+#define JOUEUR_H
+
+
+class joueur
+{
+public:
+    joueur();
+    int getpoints(){return points;}
+    void incrementerpoints(int pointsenplus){points = points + pointsenplus;}
+
+    void losevie(){nbboulesrestantes--;}
+    int getnbboules(){return nbboulesrestantes;}
+    void initnbvie(){nbboulesrestantes = 3;}
+
+    void incrementerniveau(){niveau++;}
+    int getniveau(){return niveau;}
+
+private :
+    int niveau = 0;
+    int points = 0;
+    int nbboulesrestantes = 3;
+};
+
+#endif // JOUEUR_H
